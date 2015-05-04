ebaee8d417e210ce68753c8f0d1e4793cdacfe72
Cory Spitzer & Michael Yanchar <coryspitzer@gmail.com>
2015-05-04 14:58:59 -0700

    Test and write #completed

--- lib/task.rb
+++ lib/task.rb
@@ -1,6 +1,9 @@
 class Task
-  attr_accessor :description
+  attr_accessor :description, :completed
+
   define_method(:initialize) do
     @description = nil
+    @completed = false
   end
+
 end
--- spec/task_spec.rb
+++ spec/task_spec.rb
@@ -3,10 +3,18 @@ require('task')
 
 describe(Task) do
   describe('#description') do
-    it('sets and gets a description') do  
+    it('sets and gets a description') do
       drink_beer = Task.new()
       drink_beer.description = 'Drink beer!'
       expect(drink_beer.description).to(eq('Drink beer!'))
+    end
+  end
+
+  describe('#completed') do
+    it('sets and gets the boolean value of completed') do
+      drink_beer = Task.new()
+      drink_beer.completed = true
+      expect(drink_beer.completed).to(eq(true))
     end
   end
 end
