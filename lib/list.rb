98342a3f3b44bc5e92b0149f5c59cd3da2c79f1b
Cory Spitzer & ian bevel <coryspitzer@gmail.com>
2015-05-05 11:42:54 -0700

    Test and define #id

--- lib/list.rb
+++ lib/list.rb
@@ -1,8 +1,9 @@
 class List
-  attr_accessor :title
+  attr_accessor :title, :id
 
   define_method(:initialize) do |params|
     @title = params.fetch(:title)
+    @id = params.fetch(:id)
   end
 
 end
--- spec/list_spec.rb
+++ spec/list_spec.rb
@@ -5,9 +5,17 @@ require('list')
 describe(List) do
   describe('#title') do
     it('gets or sets the title') do
-      list = List.new({:title => "Groceries"})
+      list = List.new({:title => "Groceries", :id => nil})
       list.title = "food"
       expect(list.title).to(eq("food"))
+    end
+  end
+
+  describe('#id') do
+    it('will get and set id') do
+      list = List.new({:title => 'Groceries', :id => nil})
+      list.id = 1
+      expect(list.id).to(eq(1))
     end
   end
 
