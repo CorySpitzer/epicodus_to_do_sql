4977451011cb868fb27ff8636d39acf982289ccf
Cory Spitzer & Jim Patt <CorySpitzer@gmail.com>
2015-05-11 13:53:02 -0700

    Test and define Task#list

--- spec/list_spec.rb
+++ spec/list_spec.rb
@@ -2,12 +2,10 @@ require('spec_helper')
 
 
 describe(List) do
-  describe('#tasks') do
-    it('tells which are in the list') do
-      list = List.create({name: "list"})
-      task1 = Task.create({ description: "task1", list_id: list_id})
-      task2 = Task.create({ description: "task2", list_id: list_id})
-      expect(list.tasks()).to(eq([task1, task2]))
-    end
+  it('tells which are in the list') do
+    list = List.create({name: "list"})
+    task1 = Task.create({ description: "task1", list_id: list.id()})
+    task2 = Task.create({ description: "task2", list_id: list.id()})
+    expect(list.tasks()).to(eq([task1, task2]))
   end
 end
--- spec/task_spec.rb
+++ spec/task_spec.rb
@@ -2,6 +2,12 @@ require('spec_helper')
 
 describe(Task) do
 
+  it("tells which list it belongs to") do
+    list = List.create({name: "list"})
+    task = Task.create({description: "task", list_id: list.id()})
+    expect(task.list()).to(eq(list))
+  end
+
   describe(".not_done") do
     it("returns the not done tasks") do
       undone1 = Task.create({description: "gotta do it", done: false})
@@ -11,4 +17,5 @@ describe(Task) do
       expect(Task.not_done()).to(eq(undone_tasks))
     end
   end
+
 end
